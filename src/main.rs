8f6f2a1379995aa5b0e87bda795fbe374eab8dce
nannany <ymym1990ymym@gmail.com>
2021-02-28 17:07:09 +0900

    チュートリアル1.3

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::from_args();
+    let result = std::fs::read_to_string(&args.path);
+
+    let content = match result {
+        Ok(content) => { content }
+        Err(error) => { panic!("can't deal {}", error); }
+    };
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+}
