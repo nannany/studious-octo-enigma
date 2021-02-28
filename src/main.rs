af9e3a9e1670ad4347bac32842c197ed9d94618c
nannany <ymym1990ymym@gmail.com>
2021-02-28 18:49:28 +0900

    なんとなく完成

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,34 @@
 use structopt::StructOpt;
+use rand::Rng;
 
 #[derive(StructOpt)]
 struct Cli {
-    pattern: String,
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
+    #[structopt(long, short, default_value = "10")]
+    size: i32,
+    #[structopt(long, short, default_value = "normal")]
+    complexity: String,
 }
+
+const NORMAL: [&str; 62] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ];
+const HARD: [&str; 84] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    "!", "#", "$", "%", "&", "(", ")", "=", "|", ".", ";", "[", "]", "/", "<", ">", ":", "@", "?", "_", "'", "\""];
 
 fn main() {
     let args = Cli::from_args();
-    let result = std::fs::read_to_string(&args.path);
-
-    let content = match result {
-        Ok(content) => { content }
-        Err(error) => { panic!("can't deal {}", error); }
-    };
-
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+    let mut password = "".to_string();
+    let mut rng = rand::thread_rng();
+    if &args.complexity == "hard" {
+        for i in 0..args.size {
+            password += HARD[rng.gen_range(0..HARD.len())];
+        }
+    } else {
+        for i in 0..args.size {
+            password += NORMAL[rng.gen_range(0..NORMAL.len())];
         }
     }
+    println!("{}", password);
 }
